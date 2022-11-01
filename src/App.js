270abe12336922eb91489e907d7d992a1d67a58b
Mimi Damnjanovic <mdamnjan@Mimis-MacBook-Pro.local>
2022-10-31 22:06:02 -0400

    Make website single page, change colors

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import "./App.css";
 import Home from "./components/Home";
 import Resume from "./components/Resume/Resume";
 import NavBar from "./components/NavBar/NavBar";
+import Project from "./components/Projects/Project"
 import Projects from "./components/Projects/Projects";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <header className="App-header">
           <NavBar />
-          <Routes>
-            <Route path={"/about-me"} element={<Home />} />
-            <Route path={"/"} element={<Home />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
         </header>
+          <img
+            alt="The character No Face from the move Spirited Away, holding a steaming cup of tea."
+            src="/noface.png"
+          />
+          <h1 id='about-me'>About Me</h1>
+          <p>
+            Hi! I'm Mimi, a student at the University of Toronto, currently
+            finishing up a computer science specialist and physics minor.
+          </p>
+          <button id="resume"><h2>Resume</h2></button>
+          <h1 id="projects">Projects</h1>
+          <Projects/>
+        <footer>
+          <div className="contacts">
+            <h2>Get in touch:</h2>{" "}
+            <a aria-label="email" href="mailto:mimi.damnjanovic16@gmail.com">
+              <i class="bi bi-envelope"></i>
+            </a>
+            <a
+              arial-label="github"
+              href="https://github.com/mdamnjan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-github"></i>
+            </a>
+            <a
+              aria-label="linkedin"
+              href="https://ca.linkedin.com/in/mimi-damnjanovic-b1a21b206"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="bi bi-linkedin"></i>
+            </a>
+          </div>
+        </footer>
       </div>
-    </Router>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,15 @@
-import { useLocation } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
-  const location = useLocation().pathname;
   const navBarItems = [
-    { display: "About Me", path: "/#/about-me" },
-    { display: "Resume", path: "/#/resume" },
-    { display: "Projects", path: "/#/projects" },
+    { display: "About Me", path: "/#about-me" },
+    { display: "Resume", path: "/#resume" },
+    { display: "Projects", path: "/#projects" },
   ].map((item) => {
     let className =
-      location === item.path ? "current-navbar navbar-item" : "navbar-item";
+      window.location === item.path ? "current-navbar navbar-item" : "navbar-item";
     return (
-      <a key={`${item.path}`}href={item.path} className={className}>
+      <a key={`${item.path}`} href={item.path} className={className}>
         {item.display}
       </a>
     );
