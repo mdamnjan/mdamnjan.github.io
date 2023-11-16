82b348559e672519ae3dbab47ece8bf1cb4288a5
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-11-16 13:26:23 -0500

    Add nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,26 @@
 const NavBar = () => {
-    
-
   return (
-    <nav class="block mb-[100px]">
-      <ul class="flex gap-6 float-right">
-        <li>
-          <a href="#about-me">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <header class="bg-blue-500 h-[40px]">
+      <nav class="block mb-[100px]">
+        <ul class="flex gap-6 float-right">
+          <li>
+            <a href="#about-me">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 export default NavBar;
