a11560c0409fb9ba972ce5d29f61c32c87f1c0d6
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-11-16 05:03:04 -0500

    Add projects, work experience, contacts sections

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+const NavBar = () => {
+    
+
+  return (
+    <nav class="block mb-[100px]">
+      <ul class="flex gap-6 float-right">
+        <li>
+          <a href="#about-me">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default NavBar;
