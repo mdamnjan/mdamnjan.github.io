eb84bf9bc19878a235f9e70116091b1f070b1cff
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-11-23 16:43:39 -0500

    Update ReadMe and descriptions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'aquamarine': 'rgb(127, 255, 212)'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        aquamarine: "rgb(127, 255, 212)",
+      },
+      fontSize: {
+        h2: "2rem",
+      },
+      fontWeight: {
+        h2: 500,
+      },
+      lineHeight: {
+        h1: 1,
+      },
+    },
+  },
+  plugins: [],
+};
